227d7a7b7dad9f105a99576a1eb53a39e1404750
sujata-c <sujata.c3001@gmail.com>
2021-01-15 14:32:40 +0530

    review commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Evaluation-Criteria-Assignment-7
+# Knowledge of Data Compression, Archiving, Marshalling, Cryptograpy and Networking
+
+
+**Problem Definition**
+
+Define Client and Server using Python Networking
+Client should read data from the file, serialize the data and write into another file.
+Encrypt the file using Python Cryptography module.
+Transfer compressed database file over the network using Python code.
+Server should decrypt the file and deserialize the data and Read data send by client
+
+**Python Networking**
+
--- server.py
+++ server.py
@@ -57,23 +57,6 @@ def recieve():
             # convert to integer
             filesize = int(filesize)
             print("recieved file: ",filename)
-            #if not data:
-             #       break
-              #  conn.sendall(data)
-        '''progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        with open(filename, "wb") as f:
-            for _ in progress:
-                # read 1024 bytes from the socket (receive)
-                bytes_read = conn.recv(BUFFER_SIZE)
-                if not bytes_read:
-                    # nothing is received
-                    # file transmitting is done
-                    break
-                # write to the file the bytes we just received
-                f.write(bytes_read)
-                # update the progress bar
-                progress.update(len(bytes_read))'''
-
         return filename
 
 if __name__ == "__main__":
